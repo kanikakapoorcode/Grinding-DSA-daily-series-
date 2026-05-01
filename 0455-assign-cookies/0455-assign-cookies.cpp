cc9c414ed86805394d7afe3b488822e7c10e7ac4
Kanika Kapoor <kanikakr823@gmail.com>
2026-05-01 16:54:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    int solve(int i , int j,vector<int>& g, vector<int>& s){
+        if (i >= g.size() || j >= s.size()) return 0;
+        int notTake = solve(i, j + 1, g, s);
+        int take = 0;
+        if (s[j] >= g[i]) {
+            take = 1 + solve(i + 1, j + 1, g, s);
+        }
+        return max(take, notTake);
+    }
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int m = g.size();
-        int n = s.size();
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-
-        int l = 0, r = 0;
-
-        while (l < n && r < m) {  
-            if (g[r] <= s[l]) {
-                r++;
-            }
-            l++;
-        }
-        return r;
+        return solve(0,0,g,s);
     }
 };
